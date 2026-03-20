624efa9c920065914f54666c5263fdbad026c1bc
Stephen Hope <53706288+stephenh67@users.noreply.github.com>
2026-03-20 18:12:19 -0400

    Create alerts.py

---
+++ src/alerts.py
@@ -0,0 +1,6 @@
+ALERTS = {
+    "ESCALATE": {
+        "en": "ESCALATE. Ratification required by CUSTODIAN (National Defence).",
+        "fr": "ESCALADER. Ratification requise par le DÉPOSITAIRE (Défense nationale)."
+    }
+}
